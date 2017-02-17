d5dda3fe63f1e6930a90123619a8504439c79f13
Naomi Mine <naomi.mine@gmail.com>
2017-02-17 15:30:13 -0600

    made bunny and messed with visibility

--- js/app.js
+++ js/app.js
@@ -1,27 +1,71 @@
 console.log("hooked upp")
 
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-
-// var picture = {
-// 	rectangle: function(){
-// 		ctx.fillStyle = "#DCE9E9"
-// 		ctx.fillRect(100, 100, 300, 300)
-// 	},
-// 	circle: function(){
-// 		ctx.arc(400, 200, 150, 0, Math.PI*2, true);
-// 		ctx.fillStyle = "#DCE9E9"
-// 		ctx.fill()
-// 		ctx.beginPath()
+// var canvas = document.getElementById('canvas');
+// var ctx = canvas.getContext('2d');
+// var drawCircles = function(){
+// 	for(i=0; i<100; i++){
+// 		var cordx = Math.random()*800;
+// 		var cordy = Math.random()*600;
+// 		ctx.arc(cordx, cordy, 10, 0, Math.PI * 2, true);
+// 		if( (cordx < 100)&&(cordy < 100) ||
+// 			(cordx < 100)&&(100 < cordy < 400) ||
+// 			(cordx < 100)&&(cordy > 400) ||
+// 			(cordx > 100)&&(cordy<100) ||
+// 			(cordx > 100)&&(cordy > 400) ||
+// 			(cordx > 400)&&(100 < cordy < 400)
+// 		){
+// 			ctx.fillStyle = "green"
+// 		} else {
+// 			ctx.fillStyle = "blue"
+// 		}
+// 		ctx.fill();
+// 		ctx.beginPath();
 // 	}
 //
 // }
-//
-var rectangle = function(){
-	ctx.fillStyle = "#DCE9E9"
-	ctx.fillRect(100, 100, 300, 300)
+
+var picture = {
+	rectangle: function(){
+		ctx.fillStyle = "#DCE9E9"
+		ctx.fillRect(100, 100, 300, 300)
+	},
+	circle: function(){
+		ctx.arc(400, 200, 150, 0, Math.PI*2, true);
+		ctx.fillStyle = "#DCE9E9"
+		ctx.fill()
+		ctx.beginPath()
+	},
+	bunny: function(){
+		//tail
+		ctx.arc(195, 380, 60, 0, Math.PI*2, true);
+		ctx.fillStyle = "#DCE9E9"
+		ctx.fill();
+		ctx.beginPath();
+		//body
+		ctx.ellipse(290, 340, 100, 120, 40 * Math.PI/150, 0, 2 * Math.PI)
+		ctx.fillStyle = "#DCE9E9"
+		ctx.fill();
+		ctx.beginPath();
+		//head
+		ctx.ellipse(370, 240, 85, 55, 45 * Math.PI/150, 0, 2 * Math.PI)
+		ctx.fillStyle = "#DCE9E9"
+		ctx.fill();
+		ctx.beginPath();
+		//ear1
+		ctx.ellipse(330, 150, 95, 30, 45 * Math.PI/105, 0, 2 * Math.PI)
+		ctx.fillStyle = "#DCE9E9"
+		ctx.fill();
+		ctx.beginPath();
+		//ear2
+		ctx.ellipse(310, 155, 92, 28, 45 * Math.PI/170, 0, 2 * Math.PI)
+		ctx.fillStyle = "#DCE9E9"
+		ctx.fill();
+		ctx.beginPath();
+	}
+
 }
-// //
+//
+
 var drawCircles = function(){
 	for(i=0; i<50; i++){
 		var cordx = Math.random()*900;
@@ -31,7 +75,8 @@ var drawCircles = function(){
 		ctx.fill();
 		ctx.beginPath();
 	}
-	rectangle()
+	picture.bunny();
+	//picture.rectangle()
 	//circle()
 }
 
@@ -58,12 +103,7 @@ var restOpacity = function(){
 // }
 
 //hide trivia q
-var showHidden = function(){
-	var hidden = document.getElementsByClassName("hidden")
-	for(i=0; i<hidden.length; i++){
-		hidden[i].style.visibility = "visible";
-	}
-}
+
 
 var nextQ = document.getElementById("nextQ-btn")
 nextQ.addEventListener("click", function(event){
@@ -84,33 +124,12 @@ nextQ.addEventListener("click", function(event){
 	showHidden();
 })
 
-// var nextQuestion = function(event){
-// 	console.log(event);
-// 	console.log("thisbuttonwasclicked")
-// 	nextQ.addEventListener("click", function(){
-// 		var decrOpacity = document.getElementsByClassName("lessOpaque");
-// 		var changeOpacity = function(){
-// 			for(i=0; i<decrOpacity.length; i++){
-// 				decrOpacity[i].style.opacity = "0.3";
-// 			}
-// 		}
-// 		var showHidden = function(){
-// 			var hidden = document.getElementsByClassName("hidden")
-// 			for(i=0; i<hidden.length; i++){
-// 				hidden[i].style.visibility = "visible";
-// 			}
-// 		}
-//
-// 	})
-// }
+
+//I want at least 5 or 6 images to construct and i need about 10 questions per picture
+//store in objects to organize code
+
+//next step: figure out how to draw more complicated things in canvas
+//list: bunny,
 
 
-//
-// //I want at least 5 or 6 images to construct and i need about 10 questions per picture
-// //store in objects to organize code
-//
-// //next step: figure out how to draw more complicated things in canvas
-// //list: bunny,
-//
-//
-// //have a function for each of these shapes/pictures, and give it a background-color that is the same as the canvas's background color. Embed this function in the draw circles function
+//have a function for each of these shapes/pictures, and give it a background-color that is the same as the canvas's background color. Embed this function in the draw circles function
