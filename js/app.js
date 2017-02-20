17eecdb543b5e5c3086d68c073c9eb7fd6c19af1
Naomi Mine <naomi.mine@gmail.com>
2017-02-20 15:35:43 -0600

    added event listeners to answers

--- js/app.js
+++ js/app.js
@@ -8,6 +8,13 @@ var score = document.getElementsByClassName("score");
 var decrOpacity = document.getElementsByClassName("lessOpaque");
 var hidden = document.getElementsByClassName("hidden");
 var nextQuestion = document.getElementById("nextQ-btn")
+var questionHeading = document.getElementsByTagName("h1")[1];
+var answer1 = document.getElementsByTagName("li")[0];
+var answer2 = document.getElementsByTagName("li")[1];
+var answer3 = document.getElementsByTagName("li")[2];
+var answer4 = document.getElementsByTagName("li")[3];
+var hidden = document.getElementsByClassName("hidden");
+
 
 var game = {
 	score: 0,
@@ -42,6 +49,8 @@ var game = {
 	restOpacity: function(){
 		for(i=0; i<decrOpacity.length; i++){
 			decrOpacity[i].style.opacity = "1";
+		}
+		for(i=0; i<hidden.length; i++){
 			hidden[i].style.visibility = "hidden"
 		}
 	},
@@ -51,7 +60,6 @@ var game = {
 }
 
 
-
 nextQuestion.addEventListener("click", function(event){
 	console.log("clicked");
 	game.changeOpacity();
@@ -59,18 +67,66 @@ nextQuestion.addEventListener("click", function(event){
 	game.counter++;
 	for(i=0; i<questionArray.length; i++){
 		var q = game.counter - 1;
-		document.getElementsByTagName("h1")[1].innerHTML = questionArray[q];
-		document.getElementsByTagName("li")[0].innerHTML = a1Array[q];
-		document.getElementsByTagName("li")[1].innerHTML = a2Array[q];
-		document.getElementsByTagName("li")[2].innerHTML = a3Array[q];
-		document.getElementsByTagName("li")[3].innerHTML = a4Array[q];
+		questionHeading.innerHTML = questionArray[q];
+		answer1.innerHTML = a1Array[q];
+		answer2.innerHTML = a2Array[q];
+		answer3.innerHTML = a3Array[q];
+		answer4.innerHTML = a4Array[q];
 	}
 })
 
 
+answer1.addEventListener("click", function(event){
+	console.log("weeee");
+	var q = game.counter - 1;
+	if(a1Array[q] === correctAnswer[q]){
+		game.restOpacity();
+		game.drawCircles()
+		console.log("correct");
+	}
+
+})
+
+answer2.addEventListener("click", function(event){
+	console.log("wooo");
+	var q = game.counter - 1;
+	if(a2Array[q] === correctAnswer[q]){
+		console.log("correct")
+		game.restOpacity();
+		game.drawCircles()
+	} else {
+		console.log("not correct")
+	}
+})
+
+answer3.addEventListener("click", function(event){
+	console.log("waaaa");
+	var q = game.counter - 1;
+	if(a3Array[q] === correctAnswer[q]){
+		console.log("correct")
+		game.restOpacity();
+		game.drawCircles()
+	} else {
+		console.log("not correct")
+	}
+})
+
+answer4.addEventListener("click", function(event){
+	console.log("wiiii");
+	var q = game.counter - 1;
+	if(a4Array[q] === correctAnswer[q]){
+		console.log("correct")
+		game.restOpacity();
+		game.drawCircles()
+	} else {
+		console.log("not correct")
+	}
+})
 //add event listener for each answer
 //function for correct answerQ
 //function for incorrect answer
+//if correct answer and answer line up upon click, run "game.restOpacity" and "game.drawCircles"
+//
 
 
 var questionArray = ["What is three plus three?", "Is the earth tilted?"]
