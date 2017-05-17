1cceb5e15c7094078dc948e4095ddb70f58df68a
Naomi Mine <naomi.mine@gmail.com>
2017-05-17 14:35:01 -0500

    changed questions and fixed event listener bug

--- js/app2.js
+++ js/app2.js
@@ -10,7 +10,6 @@ var score = document.getElementsByClassName("score");
 var decrOpacity = document.getElementsByClassName("lessOpaque");
 var hidden = document.getElementsByClassName("hidden");
 var nextQuestion = document.getElementById("nextQ-btn");
-var newPicture = document.getElementById("identPic-btn");
 var questionHeading = document.querySelector("h1.questionHeading");
 var answer1 = document.getElementsByTagName("li")[0];
 var answer2 = document.getElementsByTagName("li")[1];
@@ -24,16 +23,17 @@ var game = {
 	score: 0,
 	trivCounter: 0,
 	picCounter: 0,
+
 	clearCanvas: function(){
 		ctx.clearRect(0, 0, canvas.width, canvas.height)
 	},
 
 	drawCircles: function() {
-			var z = game.picCounter;
-			var pic = picturesArray[z];
-			pic();
-			this.score += 10;
-			scoreCounter.innerHTML = "Points: " + game.score;
+		var z = game.picCounter;
+		var pic = picturesArray[z];
+		pic();
+		this.score += 10;
+		scoreCounter.innerHTML = "Points: " + game.score;
 	},
 
 	changeOpacity: function(){
@@ -74,33 +74,28 @@ var game = {
 };
 
 //Question Logic
-var listItems = document.getElementsByTagName("li");
-var questionh1 = document.getElementsByClassName("questionHeading")
+var listItems = $("li");
+var questionh1 = $(".questionHeading");
 
-nextQuestion.addEventListener("click", function(event){
-	console.log("clicked");
-	// game.changeOpacity();
-	// game.showHidden();
+$('#nextQ-btn').click(function(e){
 	game.addRemoveClass("hidden", ".visibility-changeable");
 	game.trivCounter++;
 	count = game.trivCounter -1
 	questionHeading.innerHTML = questionArray[count].question;
 	for(i=0; i<listItems.length; i++) {
 		listItems[i].innerHTML = questionArray[count].answers[i];
-	}
-});
+		}
+	});
+
 
 for(i=0; i<listItems.length; i++) {
-	listItems[i].addEventListener('click', function() {
+	$(listItems[i]).click(function() {
 		console.log('LI CLICKED');
 		if(this.innerHTML === questionArray[count].correctAnswer) {
-			console.log("yaaas");
-			// game.restOpacity();
-
 			game.drawCircles();
 			game.addRemoveClass("hidden", ".visibility-changeable");
 		} else {
-			console.log("nooo");
+			console.log("no");
 		}
 	});
 }
@@ -110,12 +105,12 @@ function QuestionCreator(question, correctAnswer, answers){
 	this.correctAnswer = correctAnswer || "";
 	this.answers = answers || [];
 }
-var questionArray = []
+var questionArray = [];
 
-var questionOne = new QuestionCreator("Which country has the largest population in the world?'", "C: China",  ["A: Indonesia", "B: India", "C: China", "D: Japan"])
+var questionOne = new QuestionCreator("Which best describes the behavior of an amoeba dividing into two identical daughter cells?", "C: Mitosis",  ["A: Diffusion", "B: Meiosis", "C: Mitosis", "D: Osmosis"])
 questionArray.push(questionOne);
 
-var questionTwo = new QuestionCreator("How many years will you live?", "A: Are you ever truly living?", ["A: Are you ever truly living?", "B: Two more, then you're done.", "C: 17.", "D: 115."])
+var questionTwo = new QuestionCreator("The site of protein synthesis is the", "A: smooth endoplasmic reticulum.", ["A: smooth endoplasmic reticulum.", "B: nucleus.", "C: nucleolus", "D: ribosome."])
 questionArray.push(questionTwo);
 
 var questionThree = new QuestionCreator("Potato is a modified form (outgrowth) of", "B: stem.", ["A: root.", "B: stem.", "C: leaf.", "D: fruit."])
@@ -124,7 +119,7 @@ questionArray.push(questionThree);
 var questionFour = new QuestionCreator("Ordinary table salt is sodium chloride. What is baking soda?", "D: Sodium bicarbonate", ["A: Potassium chloride", "B: Potassium carbonate", "C: Potassium hydroxide", "D: Sodium bicarbonate"])
 questionArray.push(questionFour);
 
-var questionFive = new QuestionCreator("If you'd like to buy something you don't need but don't have enough money, what should you do?", "D: Wait until you've earned enough extra cash.", ["A: Buy it with your credit card.", "B: Borrow money from a friend or family member.", "C: Get a bank loan for it.", "D: Wait until you've earned enough extra cash."])
+var questionFive = new QuestionCreator("The process by which DNA copies itself is called", "C: replication.", ["A: respiration.", "B: translation.", "C: replication.", "D: parturition."])
 questionArray.push(questionFive);
 
 var questionSix = new QuestionCreator("Plants have ? while animals lack it.", "B: Cellulose", ["A: Starch", "B: Cellulose", "C: Protein", "D: Fat"])
@@ -133,7 +128,7 @@ questionArray.push(questionSix);
 var questionSeven = new QuestionCreator("On which of the following did Gregor Mendel perform his famous experiment?", "C: Pea", ["A: Corn",  "B: Maize", "C: Pea", "D: Wheat"])
 questionArray.push(questionSeven);
 
-var questionEight = new QuestionCreator("Do you love your parents?", "C: Yes, when they let me spread my wings and fly.", ["A: No.", "B: Gross, cooties.", "C: Yes, when they let me spread my wings and fly.", "D: They are my best friends!"])
+var questionEight = new QuestionCreator("Your tongue does not grow hair because", "B: different genes are expressed on different tissues.", ["A: skin cells have extra DNA that encodes hair proteins.", "B: different genes are expressed on different tissues.", "C: the genes for hair proteins have been deleted from your tongue.", "D: saliva prevents the hair from growing."])
 questionArray.push(questionEight);
 
 var questionNine = new QuestionCreator("At what temperature does water boil in Fahrenheit?", "B: 212°", ["A: 100°", "B: 212°", "C: 273°", "D: 190°"])
@@ -142,7 +137,7 @@ questionArray.push(questionNine);
 var questionTen = new QuestionCreator("Oxygen in our blood is transported by a protein named", "A: hemoglobin.", ["A: hemoglobin.", "B: keratin.", "C: collagen.", "D: myoglobin."])
 questionArray.push(questionTen);
 
-var questionEleven = new QuestionCreator("Kool-Aid was originally marketed as", "D: Fruit Smack", ["A: Cool Stuff", "B: Nasty Taste", "C: Sugar Taste", "D: Fruit Smack"])
+var questionEleven = new QuestionCreator("If the DNA fingerprint of a suspect does not match a blood sample from a crime scene, what can you conclude?", "D: The blood sample had to come from another person but the suspect still may have been there", ["A: The suspect was never at the crime scene", "B: The suspect may have been at the crime scene because DNA fingerprinting still may have been there", "C: You cannot get a DNA fingerprint from a blood sample", "D: The blood sample had to come from another person but the suspect still may have been there"])
 questionArray.push(questionEleven)
 
 var questionTwelve = new QuestionCreator("Bacterial infections in humans can be treated with what?", "C: Antibiotics", ["A: Methamphetamines", "B: Steroids", "C: Antibiotics", "D: Ibuprofin"])
@@ -157,19 +152,19 @@ questionArray.push(questionFourteen);
 var questionFifteen = new QuestionCreator("What food group has the highest levels of protein?", "C: Meat", ["A: Bread", "B: Vegetables", "C: Meat", "D: Fruit"])
 questionArray.push(questionFifteen);
 
-var questionSixteen = new QuestionCreator("What are some of the best ways to protect yourself against unexpected, catastrophic losses?", "D: All of the above", ["A: Minimizing debt", "B: Having health, home/renters, and auto insurance", "C: Having multiple sources of income", "D: All of the above"])
+var questionSixteen = new QuestionCreator("In science, the term theory generally applies to an idea that", "B: attempts to explain many related phenomena", ["A: is a speculation lacking supportive observations or experiments", "B: attempts to explain many related phenomena", "C: is synonymous with what scientists call a hypothesis", "D: cannot be tested"])
 questionArray.push(questionSixteen);
 
 var questionSeventeen = new QuestionCreator("Plants receive their nutrients mainly from", "C: soil.", ["A: clorophyll.", "B: the atmosphere.", "C: soil.", "D: light."])
 questionArray.push(questionSeventeen);
 
-var questionEighteen = new QuestionCreator("What percentage of people are left-handed?", "A: 10%", ["A: 10%", "B: 17%", "C: 25%", "D: 35%"])
+var questionEighteen = new QuestionCreator("What type of variation in finches is passed to the offspring?", "C: All characteristics that are genetically determined", ["A: Any learned behaviors", "B: Only characteristics that were beneficial during a finch’s lifetime", "C: All characteristics that are genetically determined", "D: Any characteristics that were positively influenced by the environment during a finch’s lifetime"])
 questionArray.push(questionEighteen)
 
 var questionNineteen = new QuestionCreator("Botany is the study of ", "D: plants.", ["A: animals.", "B: culture.", "C: arachnids.", "D: plants."])
 questionArray.push(questionNineteen);
 
-var questionTwenty = new QuestionCreator("Which of the following items was owned by the fewest U.S. homes in 1990?", "B: Compact disc player", ["A: Computer", "B: Compact disc player", "C: Cordless phone", "D: Dishwasher"])
+var questionTwenty = new QuestionCreator("Which characteristic of an element determines its position in the periodic table?", "A: Number of protons", ["A: Number of protons", "B: Number of neutrons", "C: Number of electrons", "D: Number of protons + number of neutrons"])
 questionArray.push(questionTwenty)
 
 var question21 = new QuestionCreator("Entomology is the science that studies", "B: insects.", ["A: behavior of human beings.", "B: insects.", "C: the origin and history of technical and scientific terms.", "D: the formation of rocks."])
@@ -183,7 +178,7 @@ var question23 = new QuestionCreator("Which of the following is considered by th
 
 //Picture Logic
 
-newPicture.addEventListener("click", function(){
+$('#pic-btn').click(function(e){
 	inputField.value = null;
 	game.addRemoveClass('hidden', '.visibility-changeable2');
 	var z = game.picCounter;
@@ -191,6 +186,8 @@ newPicture.addEventListener("click", function(){
 		z++;
 	}
 })
+
+
 
 var triangle = function(){
 	for(i=0; i<300; i++){
@@ -220,7 +217,7 @@ var bunny = function(){
 		ctx.ellipse(330, 150, 135, 33, 70 * Math.PI/150, 0, 2 * Math.PI)
 		ctx.moveTo(310, 155)
 		ctx.ellipse(310, 155, 132, 31, 45 * Math.PI/150, 0, 2 * Math.PI)
-	checkk();
+		checkk();
 	}
 }
 
@@ -235,18 +232,16 @@ var heart = function(){
 		ctx.bezierCurveTo(420, 304, 460, 260, 460, 225);
 		ctx.bezierCurveTo(460, 225, 460, 150, 400, 150);
 		ctx.bezierCurveTo(370, 150, 350, 174, 350, 180);
-		//ctx.fill();
-
-	   	checkk();
-		}
+		checkk();
+	}
 }
 
 
 var rect = function(){
 	for(i=0; i<300; i++){
-			ctx.beginPath();
-			ctx.rect(100, 100, 200, 200);
-			checkk();
+		ctx.beginPath();
+		ctx.rect(100, 100, 200, 200);
+		checkk();
 	}
 }
 
@@ -272,34 +267,31 @@ var checkk = function(){
 	var cordx = Math.random()*900;
 	var cordy = Math.random()*900;
 	if(ctx.isPointInPath(cordx, cordy)){
-			ctx.beginPath()
-			ctx.arc(cordx, cordy, 5, 0, Math.PI * 2, true);
-			ctx.fillStyle = "#021F26"
-			ctx.fill();
+		ctx.beginPath();
+		ctx.arc(cordx, cordy, 5, 0, Math.PI * 2, true);
+		ctx.fillStyle = "#021F26";
+		ctx.fill();
 	} else {
-			ctx.beginPath()
-			ctx.arc(cordx, cordy, 7, 0, Math.PI * 2, true);
-			ctx.fillStyle = "#D3D7D8"
-			ctx.fill();
-		}
+		ctx.beginPath();
+		ctx.arc(cordx, cordy, 7, 0, Math.PI * 2, true);
+		ctx.fillStyle = "#D3D7D8";
+		ctx.fill();
 	}
+}
 
 
 var inputField = document.getElementsByClassName("guessPic")[0];
 var submitGuess = document.getElementsByClassName("guessPic")[1];
 var correctPic = function() {
 	game.addRemoveClass('hidden', '.visibility-changeable2');
-	// inputField.style.visibility = "hidden";
-	// submitGuess.style.visibility = "hidden";
 	game.clearCanvas();
 	game.picCounter++;
 }
 
 
-var picturesArray = [heart, triangle, bunny, flower, rect]
-
-submitGuess.addEventListener("click", function(event){
-	console.log("cluck")
+var picturesArray = [heart, bunny, flower, rect, triangle]
+//make case insensitive, make DRY
+$('#submitGuess').click(function(e) {
 	if(game.picCounter === 0){
 		if(inputField.value === "heart" || inputField.value === "love" || inputField.value === "Heart"){
 			correctPic();
@@ -307,29 +299,29 @@ submitGuess.addEventListener("click", function(event){
 			scoreCounter.innerHTML = "Points: " + game.score;
 		} else console.log("nah uh");
 	} else if(game.picCounter === 1) {
-		if(inputField.value === "triangle" || inputField.value === "Triangle"){
-			correctPic();
-			game.score += 100;
-			scoreCounter.innerHTML = "Points: " + game.score;
-		} else console.log("nah uh");
-	} else if(game.picCounter === 2){
 		if(inputField.value === "bunny" || inputField.value === "rabbit" || inputField.value === "Bunny" || inputField.value === "Rabbit"){
 			correctPic();
 			game.score += 100;
 			scoreCounter.innerHTML = "Points: " + game.score;
 		} else console.log("nah uh");
-	} else if(game.picCounter === 3){
+	} else if(game.picCounter === 2){
 		if(inputField.value === "flower" || inputField.value === "plant" || inputField.value === "Flower" || inputField.value === "Plant"){
 			correctPic();
 			game.score += 100;
 			scoreCounter.innerHTML = "Points: " + game.score;
 		} else console.log("nah uh");
-	} else if(game.picCounter === 4){
+	} else if(game.picCounter === 3){
 		if(inputField.value === "rectangle" || inputField.value === "square" || inputField.value === "Rectangle" || inputField.value === "Square"){
 			correctPic();
 			game.score += 100;
 			scoreCounter.innerHTML = "Points: " + game.score;
 		} else console.log("nah uh");
+	} else if(game.picCounter === 4){
+		if(inputField.value === "triangle" || inputField.value === "Triangle"){
+			correctPic();
+			game.score += 100;
+			scoreCounter.innerHTML = "Points: " + game.score;
+		} else console.log("nah uh");
 	}
-
 })
+
